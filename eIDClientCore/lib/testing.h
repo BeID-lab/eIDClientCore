31153f63bcdc9b75d764937c19cba8bac7f654d0
Frank Morgner <Frank.Morgner@bdr.de>
2015-11-12 11:19:51 +0100

    declare CANCEL_AFTER_PAOS_CONNECTION_ESTABLISHMENT as C symbol
    otherwise VC compilation fails

--- eIDClientCore/lib/testing.h
+++ eIDClientCore/lib/testing.h
@@ -1,1 +1,15 @@
-extern unsigned char CANCEL_AFTER_PAOS_CONNECTION_ESTABLISHMENT;
+#if !defined(__TESTING_H_INCLUDED__)
+#define __TESTING_H_INCLUDED__
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    extern unsigned char CANCEL_AFTER_PAOS_CONNECTION_ESTABLISHMENT;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
