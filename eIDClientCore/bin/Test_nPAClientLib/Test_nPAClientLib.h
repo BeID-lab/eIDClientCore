5e500f83cc395f4b3c38af37704b1441a5b2b343
Erik Nellessen <erik.nellessen@informatik.hu-berlin.de>
2015-12-02 12:04:37 +0100

    Use gengetopt to do command line argument parsing.

--- eIDClientCore/bin/Test_nPAClientLib/Test_nPAClientLib.h
+++ eIDClientCore/bin/Test_nPAClientLib/Test_nPAClientLib.h
@@ -11,17 +11,13 @@ int performEID(std::string strServiceURL,
 		
 std::string str_replace_ifnot(std::string rep, std::string wit, std::string ifnot, std::string in);
 
-#define SAML_1 1
-#define SAML_2 2
-#define NO_SAML 3
-#define SAML_SELBSTAUSKUNFT_WUERZBURG 4
-#define SAML_AUTENTAPP 5
+#include "cmdline.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-extern unsigned char SAML_VERSION;
+extern enum enum_testcase SAML_VERSION;
 extern const char *pin;
 
 #ifdef __cplusplus
